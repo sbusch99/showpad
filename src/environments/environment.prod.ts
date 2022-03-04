48133650c6efd1e8658675c35fe468db484a9d34
Steven Buschman <steven.buschman@gmail.com>
2022-03-03 22:14:56 -0500

    first push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,4 +4,8 @@ export const environment = {
   ngxLoggerLevel: NgxLoggerLevel.OFF,
   production: true,
   idleTimeout: 20 * 60 * 1000,
+  api: {
+    uri: 'https://pokeapi.co/api',
+    version: 'v2',
+  },
 };
